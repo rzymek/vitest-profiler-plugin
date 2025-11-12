7315c7c8ed81ff3752df88fc43e45affecac2c40
rzymek <krzysztof.rzymkowski@gmail.com>
2025-11-12 23:46:05 +0100

    profilerPlugin

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { fileURLToPath } from 'node:url';
+import {fileURLToPath} from 'node:url';
 import path from 'node:path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default function plugin() {
+export function profilerPlugin() {
     return {
         name: 'vitest-profiler-plugin',
         configureVitest(context) {
-            if(process.env.VITEST_PROFILER_DISABLE) {
+            if (process.env.VITEST_PROFILER_DISABLE) {
                 return;
             }
             context.project.globalConfig.setupFiles.push(__dirname + '/vitest-profiler-plugin.js');
